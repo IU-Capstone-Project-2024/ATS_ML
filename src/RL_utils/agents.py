9beb0472b45c828b5d16279d6883ec17a8c2f7c6
Daniil Abrosimov <d.abrosimov@innopolis.university>
2024-07-10 18:20:32 +0300

    custom agent defined here

--- src/RL_utils/agents.py
+++ src/RL_utils/agents.py
@@ -1,89 +1,62 @@
+from stable_baselines3.common.policies import ActorCriticPolicy
+from typing import Callable, List, Optional, Type, Union, Dict, Tuple
+import gymnasium as gym
 import torch as th
 from torch import nn
-from stable_baselines3.common.policies import ActorCriticPolicy
-from gymnasium import spaces
-from typing import Callable, Tuple
 
-class CustomTradingNetwork(nn.Module):
-    def __init__(self, feature_dim_ts: int, feature_dim_nf: int, combined_dim: int = 64):
-        super(CustomTradingNetwork, self).__init__()
-
-        # Sub-network for time series data
-        self.time_series_net = nn.Sequential(
-            nn.Conv1d(in_channels=feature_dim_ts, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(32 * self.time_series_length, 64),
-            nn.ReLU()
-        )
-
-        # Sub-network for normal features
-        self.normal_features_net = nn.Sequential(
-            nn.Linear(feature_dim_nf, 64),
-            nn.ReLU()
-        )
-
-        # Combined processing network
-        self.combined_net = nn.Sequential(
-            nn.Linear(64 + 64, combined_dim),
-            nn.ReLU()
-        )
-
-        # Separate heads for policy and value functions
-        self.policy_net = nn.Sequential(
-            nn.Linear(combined_dim, 64),
-            nn.ReLU()
-        )
-
-        self.value_net = nn.Sequential(
-            nn.Linear(combined_dim, 64),
-            nn.ReLU()
-        )
-
-        # Save output dimensions for creating distributions
-        self.latent_dim_pi = 64
-        self.latent_dim_vf = 64
-
-    def forward(self, features: Tuple[th.Tensor, th.Tensor]) -> Tuple[th.Tensor, th.Tensor]:
-        time_series_data, normal_features = features
-
-        # Process time series data
-        ts_features = self.time_series_net(time_series_data)
-
-        # Process normal features
-        nf_features = self.normal_features_net(normal_features)
-
-        # Combine the processed features
-        combined_features = th.cat((ts_features, nf_features), dim=1)
-        combined_features = self.combined_net(combined_features)
-
-        # Compute policy and value features
-        policy_features = self.policy_net(combined_features)
-        value_features = self.value_net(combined_features)
-
-        return policy_features, value_features
-
-class CustomTradingPolicy(ActorCriticPolicy):
+class CustomNetwork(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the feature extractor.
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
     def __init__(
-        self,
-        observation_space: spaces.Space,
-        action_space: spaces.Space,
-        lr_schedule: Callable[[float], float],
-        *args,
-        **kwargs,
+    self,
+    feature_dim: int,
+    last_layer_dim_pi: int = 64,
+    last_layer_dim_vf: int = 64,
     ):
+        super(CustomNetwork, self).__init__()
+        # IMPORTANT:
+        # Save output dimensions, used to create the distributions
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Policy network
+        self.policy_net = nn.Sequential(
+        nn.Linear(last_layer_dim_pi*feature_dim, last_layer_dim_pi*2),
+        nn.ReLU(),
+        nn.Linear(last_layer_dim_pi*2,last_layer_dim_pi),
+        nn.Tanh())
+
+        # Value network
+        self.value_net = nn.Sequential(
+        nn.Linear(last_layer_dim_pi*feature_dim, last_layer_dim_pi*2), 
+        nn.ReLU(),
+        nn.Linear(last_layer_dim_pi*2, last_layer_dim_pi),
+        nn.Tanh())
+    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of     the specified network.
+        If all layers are shared, then ``latent_policy == latent_value``
+        """
+        return self.policy_net(features), self.value_net(features)
+    def forward_actor(self, features: th.Tensor) -> th.Tensor:
+        return self.policy_net(features)
+    def forward_critic(self, features: th.Tensor) -> th.Tensor:
+        return self.value_net(features)
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__( self, observation_space: gym.spaces.Space, action_space: gym.spaces.Space, lr_schedule: Callable[[float], float], net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None, activation_fn: Type[nn.Module] = nn.Tanh, *args, **kwargs,):
+        super(CustomActorCriticPolicy, self).__init__( observation_space, action_space, lr_schedule, net_arch, activation_fn,
+        # Pass remaining arguments to base class
+*args,**kwargs,)
         # Disable orthogonal initialization
-        kwargs["ortho_init"] = False
-        super().__init__(
-            observation_space,
-            action_space,
-            lr_schedule,
-            *args,
-            **kwargs,
-        )
+        self.ortho_init = False
+
 
     def _build_mlp_extractor(self) -> None:
-        # Create the custom network with appropriate input dimensions
-        feature_dim_ts = self.observation_space['env_states'].shape[1]
-        feature_dim_nf = self.observation_space['agent_data'].shape[0]
-        self.mlp_extractor = CustomTradingNetwork(feature_dim_ts, feature_dim_nf)
+        self.mlp_extractor = CustomNetwork( last_layer_dim_pi = self.action_space.n, last_layer_dim_vf = self.action_space.n, feature_dim=self.observation_space.shape[0])
