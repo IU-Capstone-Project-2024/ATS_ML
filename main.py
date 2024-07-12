77bab000059592bb4131b4cdae0d573a7d790891
Daniil Abrosimov <d.abrosimov@innopolis.university>
2024-07-12 20:43:49 +0300

    now it should work with both models

--- main.py
+++ main.py
@@ -12,27 +12,50 @@ import matplotlib.pyplot as plt
 import time
 import json
 from src.easy_data_collection import get_last_data
+from src.RL_utils.Order import TestOrder
+from src.RL_utils.reward_functions import calc_profit
 
 cfg = json.load(open("configs/main.json"))
 
 
-def wrap_observations(observations, current_gain, holding_flag):
+def wrap_ordinary_observations(observations, last_trade: TestOrder):
     # Wraps the observations in the format expected by the model 
 
     if len(observations) != cfg["observation_window"]:
         raise ValueError("The number of observations must be equal to the observation window")
 
     obs = observations[cfg["observed_features"]].values.flatten()
-    obs = np.append(obs, [current_gain, holding_flag])
+    current_price = observations["Close"].values[-1]
+    if last_trade.exit_timestamp is not None:
+        obs = np.append(obs, [(current_price / last_trade.entry_price - 1) * 100, True])
+    else:
+        obs = np.append(obs, [0, False])
+    return obs
+
+
+def wrap_knife_observations(observations, last_trade: TestOrder, last_action: int):
+    # Wraps the observations in the format expected by the model 
+
+    if len(observations) != cfg["observation_window"]:
+        raise ValueError("The number of observations must be equal to the observation window")
+
+    obs = observations[cfg["observed_features"]].values.flatten()
+    current_price = observations["Close"].values[-1]
+    if last_trade.exit_timestamp is not None:
+        profitloss = calc_profit(last_trade.entry_price, current_price) - 0.1
+        obs = np.append(obs, [profitloss, last_action])
+    else:
+        obs = np.append(obs, [0, last_action])
 
     return obs
+
 
 def main():
     # Load the model
     model = PPO.load(cfg["model_path"])
     
-    current_gain = 0
-    holding_flag = 0
+    last_trade = TestOrder("BTCUSD", None, None, None, cfg["strategy"])
+    last_action = 0
 
     # Define the next prediction time
     # Round the current time to the nearest 15 seconds
@@ -48,17 +71,28 @@ def main():
             observations = get_last_data(5, "15s")
 
             # Wrap the observations
-            obs = wrap_observations(observations, current_gain, holding_flag)
+            if cfg["strategy"] == "No strategy":
+                obs = wrap_ordinary_observations(observations, last_trade)
+            elif cfg["strategy"] == "Knife":
+                obs = wrap_knife_observations(observations, last_trade, last_action)
 
             # Make a prediction
-            action, _ = model.predict(obs, deterministic=True)
+            last_action, _ = model.predict(obs, deterministic=True)
+
+            # Save the last trade
+            if last_action == 1:
+                last_trade = TestOrder("BTCUSD", observations["Close"].values[-1], observations["unix"].values[-1], "15s", cfg["strategy"])
+            elif last_action == 2:
+                last_trade.exit_price = observations["Close"].values[-1]
+                last_trade.exit_timestamp = observations["unix"].values[-1]
+                last_trade.profit = calc_profit(last_trade.entry_price, last_trade.exit_price) - 0.1
 
             # Send the action to the endpoint
             # For now, we will just print the action
             if cfg["receiver_endpoint"]:
                 pass
             else:
-                print(f"Action: {action}")
+                print(f"Action: {last_action}")
 
             # Update the next prediction time
             next_prediction_time = np.ceil(time.time() / cfg["prediction_frequency"]) * cfg["prediction_frequency"]
